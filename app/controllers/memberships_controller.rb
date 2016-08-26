d8e9bc44265fdf883e13492355aff707b6fcf0b2
Cornelius Nurimba <colli16@hotmail.com>
2016-08-26 16:23:02 +0200

    fixed page links

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -4,6 +4,7 @@ class MembershipsController < ApplicationController
     @membership = current_user.memberships.build
     @polterabends = Polterabend.all.map do |polterabend|
       [polterabend.title, polterabend.id]
+    end
   end
 
 end
