2c760d016013aa5da5c98275061a248e453cc5fd
Sara Wulff Kringelbach <sara_kringelbach@hotmail.com>
2016-08-29 19:41:41 +0200

    navbar almost done but no design and a stupid dropdown we have to fix

--- app/models/polterabend.rb
+++ app/models/polterabend.rb
@@ -4,7 +4,7 @@ class Polterabend < ApplicationRecord
 
   has_many :memberships, :dependent => :delete_all
   has_one :dayplanner, :dependent => :delete
-  has_many :activity_polterabend, :dependent => :destroy
+  has_many :activity_polterabends, :dependent => :destroy
   has_many :comments, dependent: :destroy
 
   mount_uploader :photo, PhotoUploader
