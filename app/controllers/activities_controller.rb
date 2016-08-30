7b4bf9a8ab59bf925f83a13a500fe70cc3b75096
Cornelius Nurimba <colli16@hotmail.com>
2016-08-30 17:22:07 +0200

    changed home banner to fading

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,16 @@
 class ActivitiesController < ApplicationController
 
   def index
-    @activities = Activity.all
+    @params = params
+    categories = %w(adventure fancy cheap boring gothic geeky crazy sexy girly)
+    categories = categories.select { |category| params.has_key?(category) }
+    if categories.empty?
+     @filtered_activities = Activity.all
+    else
+      @filtered_activities = Activity.all.select do |activity|
+        categories.include?(activity.category)
+      end
+    end
 
     activities = Activity.where.not(latitude: nil, longitude: nil)
     @hash = Gmaps4rails.build_markers(activities) do |activity, marker|
@@ -43,19 +52,19 @@ class ActivitiesController < ApplicationController
     end
   end
 
-  def filtered_activities
-    @params = params
-    categories = %w(adventure fancy cheap boring gothic geeky crazy sexy girly)
-    categories = categories.select { |category| params.has_key?(category) }
-    if categories.empty?
-     @filtered_activities = Activity.all
-    else
-      @filtered_activities = Activity.all.select do |activity|
-        categories.include?(activity.category)
-      end
-    end
-    render :index
-  end
+  # def filtered_activities
+  #   @params = params
+  #   categories = %w(adventure fancy cheap boring gothic geeky crazy sexy girly)
+  #   categories = categories.select { |category| params.has_key?(category) }
+  #   if categories.empty?
+  #    @filtered_activities = Activity.all
+  #   else
+  #     @filtered_activities = Activity.all.select do |activity|
+  #       categories.include?(activity.category)
+  #     end
+  #   end
+  #   render :index
+  # end
 
   private
 
