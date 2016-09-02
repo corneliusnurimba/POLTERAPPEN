edbececbb97a891e07a5b9553cc4622143ebdee9
Sara Wulff Kringelbach <sara_kringelbach@hotmail.com>
2016-09-02 13:28:43 +0200

    stupid dayplanner fuck

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ActivityDayplanner.destroy_all
 ActivityPolterabend.destroy_all
 Activity.destroy_all
 
