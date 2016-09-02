86717dd45da6bc786374a6241de17e121e5caa46
Sara Wulff Kringelbach <sara_kringelbach@hotmail.com>
2016-09-02 09:50:28 +0200

    sigup gets firstname and lastname

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users,
-    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'registrations' }
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
@@ -19,6 +20,9 @@ Rails.application.routes.draw do
     resources :activity_dayplanners
     resources :comments, only: :create
   end
+
+
+
 
   post '/save_activities', to: 'activity_polterabends#save_activities'
   post '/save_activity', to: 'activity_polterabends#save_activity'
